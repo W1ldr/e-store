dd50b6f060f4e546572e19b8d579e26dcb524535
Wilder Ribeiro <dewillribeiro@gmail.com>
2021-08-02 00:21:52 +0100

    .env

--- app.js
+++ app.js
@@ -2,3 +2,11 @@ const express = require("express");
 
 const app = express();
 
+//for local variables in .env
+//if you don't have my .env uncomment the next line
+//const api = '/api/v1'
+require("dotenv/config");
+
+const api = process.env.API_URL;
+
+console.log(api);
