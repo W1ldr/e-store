a3d3aba41a345abd277eb3ce1d0e9ff6e00fdae0
Wilder Ribeiro <dewillribeiro@gmail.com>
2021-08-02 01:32:20 +0100

    midleware

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
-const express = require("express");
+const Express = require("express");
 
-const app = express();
+const app = Express();
+const morgan = require("morgan");
+const port = 3000;
+
+//middleware
+app.use(Express.json());
+app.use(morgan("tiny"));
 
 //for local variables in .env
 //if you don't have my .env uncomment the next line
 //const api = '/api/v1'
 require("dotenv/config");
-
 const api = process.env.API_URL;
 
-console.log(api);
+app.get(`${api}/products`, (req, res) => {
+  const Products = {
+    id: 1,
+    name: "hair dresser",
+    image: "image url",
+  };
+  res.send(Products);
+});
+
+app.post(`${api}/products`, (req, res) => {
+  const newProduct = req.body;
+  console.log(newProduct);
+  res.send(newProduct);
+});
+
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+});
