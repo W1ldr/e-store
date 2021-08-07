81318795d8a5da90ce47b66f39e2f449e92f0493
Wilder Ribeiro <dewillribeiro@gmail.com>
2021-08-07 18:55:21 +0100

    up1

--- routes/categories.js
+++ routes/categories.js
@@ -1,14 +1,22 @@
-const {Category} = require('../models/category');
-const express = require('express');
+const { Category } = require("../models/category");
+const express = require("express");
 const router = express.Router();
 
-router.get(`/`, async (req, res) =>{
-    const categoryList = await Category.find();
+router.get(`/`, async (req, res) => {
+  const categoryList = await Category.find();
 
-    if(!categoryList) {
-        res.status(500).json({success: false})
-    } 
-    res.send(categoryList);
-})
+  if (!categoryList) {
+    res.status(500).json({ success: false });
+  }
+  res.send(categoryList);
+});
 
-module.exports =router;
+router.post(`/`, async (req, res) => {
+  let category = new Category({
+    name: req.body.name,
+    icon: req.body.icon,
+    color: req.body.color,
+  });
+});
+
+module.exports = router;
